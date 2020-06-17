37b011ec84443ca0ca3216eb77dfc03ec5273ba0
peterhchen <peter.hp.chen@gmail.com>
2020-06-16 21:51:53 -0700

    Train Q Learning

--- 09_train_qlearning/05_fronzen_lake.py
+++ 09_train_qlearning/05_fronzen_lake.py
@@ -77,3 +77,36 @@ print("\n\n********Q-table********\n")
 print(q_table)
 
 assert avg_reward > 0.6
+# Watch our agent play Frozen Lake by playing the best action 
+# from each state according to the Q-table.
+# Run for more episodes to watch longer.
+
+for episode in range(1):
+    state = env.reset()
+    done = False
+    print("*****EPISODE ", episode+1, "*****\n\n\n\n")
+    time.sleep(1)
+
+    for step in range(max_steps_per_episode):        
+        clear_output(wait=True)
+        env.render()
+        time.sleep(0.3)
+        
+        action = np.argmax(q_table[state,:])        
+        new_state, reward, done, info = env.step(action)
+        
+        if done:
+            clear_output(wait=True)
+            env.render()
+            if reward == 1:
+                print("****You reached the goal!****")
+                time.sleep(3)
+            else:
+                print("****You fell through a hole!****")
+                time.sleep(3)
+            clear_output(wait=True)
+            break
+            
+        state = new_state
+        
+env.close()
